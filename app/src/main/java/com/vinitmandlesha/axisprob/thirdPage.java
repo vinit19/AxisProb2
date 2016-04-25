1ed3459001b1e167fe2d797189122948cda23b3e
vinit19 <vinitmandlesha@gmail.com>
2016-04-26 02:35:09 +0530

    Cmmit

---
+++ app/src/main/java/com/vinitmandlesha/axisprob/thirdPage.java
@@ -0,0 +1,13 @@
+package com.vinitmandlesha.axisprob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class thirdPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_page);
+    }
+}
